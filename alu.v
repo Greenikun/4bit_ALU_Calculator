cc28f186391fe14ff32505b55dae057324156b1d
Greenikun <89420135+Greenikun@users.noreply.github.com>
2025-07-11 15:33:47 -0500

    alu.v

--- alu.v
+++ alu.v
@@ -1,19 +1,31 @@
-module alu (
-    input [3:0] A,
-    input [3:0] B,
-    input [2:0] sel,   // operation selector
-    output reg [3:0] result
-);
-    always @(*) begin
-        case (sel)
-            3'b000: result = A + B;       // ADD
-            3'b001: result = A - B;       // SUB
-            3'b010: result = A & B;       // AND
-            3'b011: result = A | B;       // OR
-            3'b100: result = A ^ B;       // XOR
-            3'b101: result = ~A;          // NOT A
-            3'b110: result = B;           // PASS B
-            3'b111: result = 4'b0000;     // Default 0
-        endcase
-    end
+module alu_tb;
+  reg [3:0] A, B;
+  reg [2:0] sel;
+  wire [3:0] result;
+
+  alu uut (
+    .A(A),
+    .B(B),
+    .sel(sel),
+    .result(result)
+  );
+  
+  initial begin
+    $display("A=%b B=%b SEL=%b RESULT=%b", A, B, sel, result);
+    $dumpfile("alu.vcd"); // for GTKWave
+    $dumpvars(0, alu_tb);
+
+    A = 4'b0101; B = 4'b0011;
+//Time delay = 10ms
+    sel = 3'b000; #10; $display("ADD  : %b", result);  // ADD
+    sel = 3'b001; #10; $display("SUB  : %b", result);  // SUB
+    sel = 3'b010; #10; $display("AND : %b", result);  // AND
+    sel = 3'b011; #10; $display("OR  : %b", result);  // OR
+    sel = 3'b100; #10; $display("XOR  : %b", result);  // XOR
+    sel = 3'b101; #10; $display("NOT  : %b", result);  // NOT A
+    sel = 3'b110; #10; $display("PASS A : %b", result);  // PASS B
+    sel = 3'b111; #10; $display("PASS B : %b", result);
+
+    $finish;
+  end
 endmodule
